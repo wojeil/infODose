ffb102e8583095f8d4fea9d2b16dbd70918f9bc2
Jimmy Smith <jasmith.188@gmail.com>
2020-04-21 10:59:17 -0400

    fixing

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,9 @@ $(document).ready(function () {
 
     });
 
+    var policeStation = "police"
+    //Query for google places
 
-    
+    const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=8000&keyword=" + policeStation + "&key=AIzaSyD9Ff1tE0-VCQ6xdBVIecM05QkaMPvHlGU"
+
 });
