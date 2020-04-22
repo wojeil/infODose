a7781fd3cea58e4d3e0202fa6f09bf9efaffdf6a
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-22 11:42:41 -0400

    api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
-
+const db = require("../models");
 // // you this file to code your API routes
 
 // // route: /api
 router.get("/info/:state/:county/:year", (req, res) => {
     //your code here
+    db.odstats.findOne({
+        where: {
+            State:req.params.state,
+            County:req.params.county,
+            Year:req.params.year,
+        }
+    }).then(function(dbstats){
+        res.json(dbstats);
+    });
 
-
-
-
-
-
-
-})
+});
 
 module.exports = router;
