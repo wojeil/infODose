6fefcd91a045afb800caf55e56974e6278057ebf
Jimmy Smith <jasmith.188@gmail.com>
2020-04-23 10:55:01 -0400

    change the api key

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     var policeStation = "police"
     //Query for google places
 
-    const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=8000&keyword=" + policeStation + "&key=AIzaSyD9Ff1tE0-VCQ6xdBVIecM05QkaMPvHlGU"
+    const queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=8000&keyword=" + policeStation + "&key=AIzaSyAHOQKkfyQvRri-GzRByO3UPvP63mil_wU"
 
 
     $.ajax({
